8659c76d53ae63964968b0925abddb4617988749
nikhiljoshi858 <nikhiljoshi858@gmail.com>
2019-09-22 17:30:02 +0530

    Meassages

---
+++ resources/views/user/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        @csrf
+        <form action="" >
+            <label for="name">Name : </label>
+            <input type="text" placeholder="Enter name here">
+            <label for="email">Email ID : </label>
+            <input type="email" placeholder="Enter email here">
+            <label for="password">Password : </label>
+            <input type="text" placeholder="Enter Password here">
+            <label for="role">Role</label>
+
+        </form>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,5 @@ Auth::routes();
 
 
 Route::get('/home', 'HomeController@index')->name('home');
-
+Route::resource('/user/index','UserController');
 Route::resource('/departments','DepartmentsController');
