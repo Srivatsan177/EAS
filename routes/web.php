858badd2d49bfa631f2de6a92841cb5de1046024
Srivatsan177 <ksrivatsan177@gmail.com>
2019-09-22 21:27:51 +0530

    final night

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,9 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::resource('/departments','DepartmentsController');
+
+Route::get('/departments/{dept_id}/teams/','TeamsController@index');
+
+Route::get('/departments/{dept_id}/teams/create','TeamsController@create');
+
+Route::post('/departments/{dept_id}/teams/store','TeamsController@store');
