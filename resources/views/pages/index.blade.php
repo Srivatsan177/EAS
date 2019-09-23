932c7b049e288ee9e9364dedf6d46030942f63c6
nikhiljoshi858 <nikhiljoshi858@gmail.com>
2019-09-23 09:53:38 +0530

    FrontEnd

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,1 +1,11 @@
-hello index
+@extends('layouts/app')
+
+@section('content')
+<!-- Begin page content -->
+<main role="main" class="flex-shrink-0">
+  <div class="container">
+    <h1>TEXT HERE</h1>
+  </div>
+</main>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
 Auth::routes();
 
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/index', 'PagesController@index')->name('home');
 
 Route::resource('/departments','DepartmentsController');
 
